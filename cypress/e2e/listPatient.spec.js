85dcb4718c9371b92248e2c96d0b0a092d59f311
AnanthaKannan <sreeananthakannan@gmail.com>
2023-09-16 16:06:00 +0530

    list patient

--- cypress/e2e/listPatient.spec.js
+++ cypress/e2e/listPatient.spec.js
@@ -6,11 +6,15 @@
 describe('List Patient', () => {
   before(() => {
     cy.intercept('GET', `${Cypress.env('apiUrl')}/patient`).as('getPatientList')
+   
     cy.login();
     cy.get('#list-patient').click()
+    cy.wait('@getPatientList')
+    cy.wait(1000); // Wait for 1 seconds
   });
 
   beforeEach(() => {
+    cy.get('.ag-body-horizontal-scroll-viewport').scrollTo('right')
     cy.restoreLocalStorage();
   });
 
@@ -18,13 +22,14 @@ describe('List Patient', () => {
     cy.saveLocalStorage();
   });
 
-  it('Patient Detail view popup model should open', () => {
-    cy.wait('@getPatientList')
-    cy.get('.ag-body-horizontal-scroll-viewport').scrollTo('right')
-    cy.wait(200)
+  it('should render ag-gird', () => {
+    // ensures that the ag-grid has rendered
+    cy.get('.ag-root-wrapper').should('be.visible');
+  });
 
+  it('Patient Detail view popup model should open', () => {
     // click the view  
-    cy.get('.ag-row-hover [aria-colindex="9"]').should('be.visible').click({ multiple: true });
+    cy.get('[aria-rowindex="3"] > [aria-colindex="9"] > #view-').should('be.visible').click({ multiple: true });
     cy.get('#popupModel').should('exist')
     cy.get('#mui-3').contains('Patient Details');
 
@@ -38,4 +43,32 @@ describe('List Patient', () => {
     cy.get('.MuiButtonBase-root > .pointer > path').click()
     cy.get('#popupModel').should('not.exist')
   });
+
+  context('delete popup', () => {
+    before(() => {
+      cy.intercept('DELETE', `${Cypress.env('apiUrl')}/patient/*`).as('deletePatient')
+      cy.intercept('GET', `${Cypress.env('apiUrl')}/patient`).as('getPatientListAfterDelete')
+    })
+
+    beforeEach(() => {
+      // click the delete button on the first row
+      cy.get('.ag-row-first > [aria-colindex="11"]').should('be.visible').click({ multiple: true });
+      cy.get('.swal2-popup').should('exist');
+    });
+
+    it('should not delete patient', () => {
+      cy.get('.swal2-cancel').click()
+      cy.get('.swal2-popup').should('not.exist');
+    })
+
+    it('should delete patient', () => {
+      cy.get('.swal2-confirm').click()
+
+      cy.wait('@deletePatient')
+      cy.wait('@getPatientListAfterDelete')
+
+      cy.get('.swal2-popup').should('not.exist');
+      cy.get('.Toastify__toast-body').should('contain', 'Patient deleted successfully')
+    })
+  })
 })
