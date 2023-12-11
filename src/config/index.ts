ce7c4d06ec0087abcd10c15c67f7e291b937746a
AnanthaKannan <sreeananthakannan@gmail.com>
2023-12-11 22:02:22 +0530

    config value updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -15,7 +15,7 @@ export const config: configType = {
   doctor: 'doctor',
   poolData: {
     UserPoolId: process.env.REACT_APP_USER_POOL_ID,
-    ClientId: process.env.REACT_APP_CLIENT_Id,
+    ClientId: process.env.REACT_APP_CLIENT_ID,
     region: process.env.REACT_APP_REGION,
   },
 };
