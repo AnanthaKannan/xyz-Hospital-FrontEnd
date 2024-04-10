9614eb58a5c12273c81c25fd06f0af12b832ab0a
AnanthaKannan <sreeananthakannan@gmail.com>
2024-04-11 00:50:16 +0530

    lazy loading added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import { Suspense } from 'react';
 
 import routes from './Router';
+import Container from './reusable/Container';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
@@ -19,15 +20,24 @@ function App() {
       <ToastContainer />
       <Routes>
         {
-       routes.map((route) => <Route key={route.PATH} path={route.PATH} 
-       element={ <Suspense fallback="Loading...."> { route.COMPONENT } </Suspense> } 
-       />)
-     }
+          routes.map((route) => <Route key={route.PATH} path={route.PATH}
+            element={
+              route.NAV ?
+                <Container title={route.TITLE}>
+                  <Suspense fallback="Loading....">
+                    {route.COMPONENT}
+                  </Suspense>
+                </Container>
+                :
+                <Suspense fallback="Loading....">
+                  {route.COMPONENT}
+                </Suspense>
+            }
+          />)
+        }
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
